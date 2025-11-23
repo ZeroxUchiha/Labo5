1f3e40b135ea3bb7eed949b08e26fda7480fe9e3
Pierre-carl Gagnon <Zeroxyde2003@gmail.com>
2025-11-23 15:56:24 -0500

    Start
    test

---
+++ src/ImageViewerApp.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class ImageViewerApp {
+    private JFrame mainFrame;
+    private JMenuBar menuBar;
+    private ImageModel imageModel;
+    private perspectives
+
+}
