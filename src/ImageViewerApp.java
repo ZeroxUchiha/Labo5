3df4bb3be627f54b83d6277c791d46771f3efa13
Pierre-carl Gagnon <Zeroxyde2003@gmail.com>
2025-11-24 10:46:08 -0500

    Push

--- src/ImageViewerApp.java
+++ src/ImageViewerApp.java
@@ -1,9 +1,15 @@
 import javax.swing.*;
+import java.util.List;
 
 public class ImageViewerApp {
     private JFrame mainFrame;
     private JMenuBar menuBar;
     private ImageModel imageModel;
-    private perspectives
+    private List<PerspectiveModel> perspectives;
+    private Serializer serializer;
+    private List<ThumbnailView> thumbnailViews;
+    private List<PerspectiveView> PerspectiveView;
+    private List<PerspectiveController> controllers;
+
 
 }
---
+++ src/ThumbnailView.java
@@ -0,0 +1,2 @@
+public class ThumbnailView {
+}
