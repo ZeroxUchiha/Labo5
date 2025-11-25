6c97b2a414f297d5967e43b524178aad6104260a
Pierre-carl Gagnon <Zeroxyde2003@gmail.com>
2025-11-24 21:31:45 -0500

    observer

--- src/ImageModel.java
+++ src/ImageModel.java
@@ -8,11 +8,6 @@ import java.util.ArrayList;
 
 
 public class ImageModel extends Observable implements Serializable {
-    private ArrayList<Observer> observerList;
-
-
-    public void notifyObservers(){}
-    public void attach(Observer o){}
-    public void dettach(Observer o){}
+    
 
 }
--- src/Observer.java
+++ src/Observer.java
@@ -1,2 +1,10 @@
+import java.util.ArrayList;
+
 public class Observer {
+    private ArrayList<Observer> observerList;
+
+
+    public void notifyObservers(){}
+    public void attach(Observer o){}
+    public void dettach(Observer o){}
 }
