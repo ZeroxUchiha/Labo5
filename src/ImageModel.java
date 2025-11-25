72240c8cc4d652b7fff2bcf6eafe30c9989dc2b8
Pierre-carl Gagnon <Zeroxyde2003@gmail.com>
2025-11-24 21:29:50 -0500

    Implement observer pattern base classes and refactor app
    Added Observable and Observer base classes to support the observer pattern. Refactored ImageModel to extend Observable and added observer management methods. Updated ImageViewerApp with initialization logic, menu bar creation, and main interface setup. Removed Main.java as entry point is now in ImageViewerApp.

--- src/ImageModel.java
+++ src/ImageModel.java
@@ -1,2 +1,18 @@
-public class ImageModel {
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+
+public class ImageModel extends Observable implements Serializable {
+    private ArrayList<Observer> observerList;
+
+
+    public void notifyObservers(){}
+    public void attach(Observer o){}
+    public void dettach(Observer o){}
+
 }
--- src/ImageViewerApp.java
+++ src/ImageViewerApp.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ImageViewerApp {
@@ -8,8 +10,107 @@ public class ImageViewerApp {
     private List<PerspectiveModel> perspectives;
     private Serializer serializer;
     private List<ThumbnailView> thumbnailViews;
-    private List<PerspectiveView> PerspectiveView;
+    private List<PerspectiveView> perspectiveViews;
     private List<PerspectiveController> controllers;
+
+
+    public ImageViewerApp(){
+        // Initialisation des modèles
+        this.imageModel = new ImageModel();
+        this.perspectives = new ArrayList<>();
+
+        // Initialisation des vues et contrôleurs
+        this.thumbnailViews = new ArrayList<>();
+        this.perspectiveViews = new ArrayList<>();
+        this.controllers = new ArrayList<>();
+
+        // Gestionnaire de sérialisation
+        this.serializer = new Serializer();
+
+        // Création de l'interface
+        createMainInterface();
+        createMenuBar();
+
+    }
+    /**
+     * Création de la fenêtre principale
+     */
+    public void createMainInterface(){
+        mainFrame = new JFrame("Image Viewer App");
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setSize(1200, 800);
+        mainFrame.setLayout(new BorderLayout());
+
+        JPanel center = new JPanel();
+        center.setBackground(Color.DARK_GRAY);
+
+        mainFrame.add(center, BorderLayout.CENTER);
+        mainFrame.setVisible(true);
+    }
+
+
+    public void createMenuBar(){
+      menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("Fichier");
+        JMenuItem miOpen = new JMenuItem("Ouvrir image");
+        JMenuItem miSave = new JMenuItem("Sauvegarder");
+        JMenuItem miLoad = new JMenuItem("Charger");
+        JMenuItem miExit = new JMenuItem("Quitter");
+        miOpen.addActionListener(e -> openImage());
+        miSave.addActionListener(e -> saveState());
+        miLoad.addActionListener(e -> loadState());
+        miExit.addActionListener(e -> exit());
+
+        fileMenu.add(miOpen);
+        fileMenu.add(miSave);
+        fileMenu.add(miLoad);
+        fileMenu.addSeparator();
+        fileMenu.add(miExit);
+
+        JMenu perspectiveMenu = new JMenu("Perspective");
+        JMenuItem miNewPersp = new JMenuItem("Nouvelle perspective");
+        miNewPersp.addActionListener(e -> createNewPerspective());
+        perspectiveMenu.add(miNewPersp);
+
+        menuBar.add(fileMenu);
+        menuBar.add(perspectiveMenu);
+
+        mainFrame.setJMenuBar(menuBar);
+    }
+
+    /**
+     * Ouvre une image depuis le disque de l'ordi
+     */
+    public void openImage(){
+        JFileChooser chooser = new JFileChooser();
+
+        if (chooser.showOpenDialog(mainFrame) == JFileChooser.APPROVE_OPTION) {
+            imageModel.loadImage(chooser.getSelectedFile().getAbsolutePath());
+
+            // Mise à jour des vignettes
+            for (ThumbnailView tv : thumbnailViews) {
+                tv.update();
+            }
+        }
+
+    }
+
+    public void saveState(){}
+
+    public void loadState(){}
+
+    public void createNewPerspective(){}
+
+    public void exit(){}
+
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(ImageViewerApp::new);
+    }
+
+
+
+
 
 
 }
